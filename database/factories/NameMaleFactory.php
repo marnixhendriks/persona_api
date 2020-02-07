8513fa73a26327c133232544ac1b0a95f8433dfe
Marnix Hendriks <info@opusimago.com>
2020-02-07 13:00:56 +0100

    Persona api init

---
+++ database/factories/NameMaleFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\NameMale;
+use Faker\Generator as Faker;
+
+$factory->define(NameMale::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
